2f1a6c4400d0a7cd3d97ea0d94dcfcb2b3b84925
Martin <martinbanas@interia.pl>
2024-03-18 12:03:03 +0100

    fix redirect bug

--- src/main/java/example/bank/service/CustomUserDetailsService.java
+++ src/main/java/example/bank/service/CustomUserDetailsService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-
     private final UserRepository userRepository;
 
     @Override
